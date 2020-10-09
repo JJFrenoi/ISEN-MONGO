47f5bc157e08ab0fae29f9f8ef1f11c62f3c5282
jean <jean.frenoi59@laposte.net>
2020-10-09 17:11:22 +0200

    j

--- PushApitoBdd.py
+++ PushApitoBdd.py
@@ -9,17 +9,29 @@ class PushApitoBdd:
         self.client = MongoClient(
             "mongodb+srv://jeanBis:123jean@cluster0.leyqh.gcp.mongodb.net/BicycleStations?retryWrites=true&w=majority")
         self.db = self.client.BicycleStations
+        self.collectionList = self.db.list_collection_names()
+        for col in self.collectionList:
+            print(col)
 
     def push(self, dataUrl, ville):
         payload = {}
         headers = {}
-        response = requests.request(
-            "GET", dataUrl, headers=headers, data=payload)
-        response_json = json.loads(response.text.encode('utf8'))
-        records = response_json.get('records')
-        if ville == 'Lille':
-                collection = self.db.Lille
+        try:
+            response = requests.request(
+                "GET", dataUrl, headers=headers, data=payload)
+            response_json = json.loads(response.text.encode('utf8'))
+            if ville == 'Lille' or ville == 'Rennes' or ville == 'Paris':
+                records = response_json.get('records')
+            elif ville == 'Lyon':
+                records = response_json.get('values')
+            else:
+                return ["Ville not found"]
+            collection = self.db[ville]
+            if ville in self.collectionList:
                 collection.drop()
-                result = collection.insert_many(records)
-                return result.inserted_ids
-        return 'failed'
+            result = collection.insert_many(records)
+        except Exception as identifier:
+            print(identifier)
+            return ["failed"]
+        finally:
+            return result.inserted_ids
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from PushApitoBdd import PushApitoBdd
 if __name__ == "__main__":
     push = PushApitoBdd()
-    id = push.push("https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=300&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion",
-                   'Lille')
-    print('id: ',id)
+
+    lilleurl = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=300&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
+    rennes_temps_reel = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=etat-des-stations-le-velo-star-en-temps-reel&q=&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles"
+    reenes_statique = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=stations_vls&q="
+
+    lyon_temps_reel = "https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json?maxfeatures=100&start=1"
+    lyon_statique = "https://download.data.grandlyon.com/ws/grandlyon/pvo_patrimoine_voirie.pvostationvelov/all.json?maxfeatures=100&start=1"
+
+    paris_temps_reel = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes"
+    paris_statique = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-emplacement-des-stations&q="
+
+    id = push.push(lilleurl ,'Lille')
+    print('id: ', id[0])
+    id1 = push.push(lyon_statique , 'Lyon')
+    print ('id1', id1[0])
+    id2 = push.push(paris_statique,'Paris')
+    print('id2', id2[0])
+    id3 = push.push(reenes_statique, 'Rennes')
+    print('id3',id3[0])
